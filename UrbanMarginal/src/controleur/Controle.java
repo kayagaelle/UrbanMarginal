5e5c0c7f2f67b2552069e620aca666937904b3b1
admin <admin@10.20.130.107>
2016-09-12 12:49:31 +0200

    tp urban page 14

---
+++ UrbanMarginal/src/controleur/Controle.java
@@ -0,0 +1,38 @@
+package controleur;
+import vue.EntreeJeu;
+import java.util.*;
+
+import javax.swing.JFrame;
+public class Controle {
+	private EntreeJeu  frmEntreeJeu ;
+	
+	public static void main(String[] args) {
+		new Controle ();
+		
+	}
+	// Constructeur 
+	public Controle (){
+		this.frmEntreeJeu = new EntreeJeu(); // 
+		frmEntreeJeu.setVisible(true);
+		
+	}
+	public void evenementVue (JFrame uneFrame ,Object info ) {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ UrbanMarginal/src/vue/EntreeJeu.java
@@ -0,0 +1,87 @@
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class EntreeJeu extends JFrame {
+
+	private JPanel contentPane;
+
+	
+	
+	private void btnStart_clic(){
+		System.out.println("using start button");
+	}
+	private void btnExit_clic(){
+		System.exit(0);
+	}
+	/**
+	 * Create the frame.
+	 */
+	public EntreeJeu() {
+		setTitle("Urban Marginal");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblS = new JLabel("Start a server");
+		lblS.setBounds(27, 38, 94, 14);
+		contentPane.add(lblS);
+		
+		JLabel lblConnectAnExisting = new JLabel("Connect an existing server :");
+		lblConnectAnExisting.setBounds(27, 82, 194, 14);
+		contentPane.add(lblConnectAnExisting);
+		
+		JLabel lblIpServer = new JLabel(" IP Server :");
+		lblIpServer.setBounds(27, 122, 94, 14);
+		contentPane.add(lblIpServer);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(130, 117, 89, 22);
+		contentPane.add(textArea);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				btnStart_clic();
+			}
+		});
+		btnStart.setBounds(229, 34, 89, 23);
+		contentPane.add(btnStart);
+		
+		
+		
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// btnConnect_clic;
+			}
+		});
+		btnConnect.setBounds(229, 118, 89, 23);
+		contentPane.add(btnConnect);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				btnExit_clic();
+			}
+		});
+		btnExit.setBounds(229, 163, 89, 23);
+		contentPane.add(btnExit);
+	}
+}
